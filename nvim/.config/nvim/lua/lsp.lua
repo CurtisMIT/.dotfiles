0a8387d1590273445371673a47fdee7b92a28b93
CurtisMIT <curtismitmit@gmail.com>
2023-07-08 10:57:19 -0400

    Migrate away from deprecated update_capabilities.

--- nvim/.config/nvim/lua/lsp.lua
+++ nvim/.config/nvim/lua/lsp.lua
@@ -10,7 +10,7 @@ local function mappings(_, bufnr)
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 local on_attach = function(client, bufnr)
   mappings(client, bufnr)
